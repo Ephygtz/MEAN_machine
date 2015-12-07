4f87f065d52ce8bd88ac26c9d57c0a79be2c45cc
Ephraim <ephygtz@gmail.com>
2015-12-07 12:01:23 +0300

    Serving files using express #method2

--- server.js
+++ server.js
@@ -1,23 +1,38 @@
-// console.log("Working to understand my MEAN stack");
-//Require the hhtp and filesysytem modules
-var http = require('http');
-var fs = require('fs');
+// // Method1 Serving files with pure node, using HTTP and FS modules without Express
+// //Require the hhtp and filesysytem modules
+// var http = require('http');
+// var fs = require('fs');
+//
+// //Create our server using the http modules
+// http.createServer(function(request, response) {
+//   //Write to our serve. Set configuration for the response
+//   response.writeHead(200, {
+//     'Content-type': 'text/html',
+//     'Access-Control-Allow-Origin' : '*'
+//   });
+//
+//   //grab the index.html file using fs
+//   var readStream = fs.createReadStream(__dirname + '/index.html');
+//
+//   //send the index.html file to our user
+//   readStream.pipe(response);
+//
+// }).listen(8888);
+//
+// //Write to the console what's happening
+// console.log('Server running on port http://127.0.0.1:8888');
 
-//Create our server using the http modules
-http.createServer(function(request, response) {
-  //Write to our serve. Set configuration for the response
-  response.writeHead(200, {
-    'Content-type': 'text/html',
-    'Access-Control-Allow-Origin' : '*'
-  });
+// Method2 Using Express
+//load Express package and create our app
+var express = require('express');
+var app = express();
+var path = require ('path');
 
-  //grab the index.html file using fs
-  var readStream = fs.createReadStream(__dirname + '/index.html');
+//send our index.html file to the user for the home package
+app.get('/', function(request, response) {
+  response.sendFile(path.join(__dirname + '/index.html'));
+});
 
-  //send the index.html file to our user
-  readStream.pipe(response);
-
-}).listen(8888);
-
-//Write to the console what's happening
-console.log('Server running on port http://127.0.0.1:8888');
+//start the server
+app.listen(8989);
+console.log('127.0.0.1:8989 is the magic port');
